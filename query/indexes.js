c6a540eaf37df890df02a00c606bc837a0758439
ArturViaderdev <93672603+ArturViaderdev@users.noreply.github.com>
2026-04-09 12:35:32 +0200

    feat: Create indexes
    Indexes.

--- query/indexes.js
+++ query/indexes.js
@@ -1,2 +1,7 @@
 //Use this file to define indexes for your queries
 db.restaurants.createIndex({"location":"2dsphere"});
+db.restaurants.createIndex({name:"text"});
+db.restaurants.createIndex({borough:1});
+db.restaurants.createIndex({cuisine:1,borough:1});
+db.restaurants.createIndex({"grades.score":1});
+db.restaurants.createIndex({"grades.grade":1,"grades.score":1});
