7f59a22c728a204c0b59f5402f6572f7ce45d1d3
ArturViaderdev <93672603+ArturViaderdev@users.noreply.github.com>
2026-04-09 12:42:37 +0200

    fix: Fixed queries
    Fixed queries

--- query/queries.js
+++ query/queries.js
@@ -89,7 +89,7 @@ db.restaurants.find({$or:[{"location.coordinates.0":{$type:"double"}},{"location
 db.restaurants.find({"grades":{$elemMatch:{"score":{$mod:[7,0]}}}},{_id:0,restaurant_id:0,name:1,"grades.grade":1});
 
 // 31. Trobar name, borough, longitud, latitud i cuisine per noms que contenen 'mon'.
-db.restaurants.find({name:{$regex:"mon",$options:"i"}},{_id:0,name:1,borough:1,location:1,cuisine:1});
+db.restaurants.find({name:{$regex:"mon",$options:"i"}},{name:1,borough:1,location:1,cuisine:1,_id:0});
 
 // 32. Mostrar restaurant_id, name i grade i score de més de 80 però menys que 100.
 db.restaurants.find({"grades":{$elemMatch:{"score":{$gt:80,$lt:100}}}},{restaurant_id:1,name:1,"grades.grade":1,"grades.score":1,_id:0});
