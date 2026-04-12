4a2d59954df2cde4c2d86ac4d7dbcfe47f8db6c5
ArturViaderdev <93672603+ArturViaderdev@users.noreply.github.com>
2026-04-12 21:03:57 +0200

    fix:Update indexes.js
    Update indexes

--- query/indexes.js
+++ query/indexes.js
@@ -6,5 +6,5 @@ db.restaurants.createIndex({"grades.score":1});
 db.restaurants.createIndex({"grades.grade":1,"grades.score":1});
 db.restaurants.createIndex({name:1});
 db.restaurants.createIndex({cuisine:1});
-db.restaurants.createIndex({address.street:1});
-db.restaurants.createIndex({location.coordinates:1});
+db.restaurants.createIndex({"address.street":1});
+db.restaurants.createIndex({"location.coordinates":1});
